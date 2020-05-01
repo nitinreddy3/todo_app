95c6a5ea83eb5ce7f3e3578835fe6109a0ead3a7
Nitin Reddy <reddynitin3@gmail.com>
2020-05-01 15:44:55 +0530

    Created a Note object with getters and setters

--- lib/widgets/note_list.dart
+++ lib/widgets/note_list.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-class NoteList extends StatelessWidget {
+class NoteList extends StatefulWidget {
+  @override
+  _NoteListState createState() => _NoteListState();
+}
+
+class _NoteListState extends State<NoteList> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('TODO List'),
-      ),
-      body: Container(
+    return Container(
 
-      ),
     );
   }
 }
